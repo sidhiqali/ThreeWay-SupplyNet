7e7c2d43ee5e50e78085bd5c660ead50f175bd25
Sidhiq Ali <sidhiqsidhiqali@gmail.com>
2023-07-09 20:41:36 +0530

    finished

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
+      backgroundImage: (theme) => ({
         hero: "url('https://res.cloudinary.com/dxp8k3sze/image/upload/v1688914311/bg-2_dx06ns.png')",
-        main: "url('https://res.cloudinary.com/dxp8k3sze/image/upload/v1688914314/main-bg_g8awxu.png)",
-      },
+        main: "url('https://res.cloudinary.com/dxp8k3sze/image/upload/v1688914314/main-bg_g8awxu.png')",
+      }),
     },
   },
   plugins: [],
